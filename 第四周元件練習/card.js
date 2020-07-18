7a4f717b4bfb4ff4da2771e66fd5fc579e20cd23
AllenDandan <64419860+AllenDandan@users.noreply.github.com>
2020-07-18 22:31:06 +0800

    元件練習問題

---
+++ 第四周元件練習/card.js
@@ -0,0 +1,9 @@
+export default{
+            
+            template: '#forCard',
+            data(){
+                return{
+                    text:'這裡有一句話',
+                }
+            }
+        }
